474e00a77026ec5cc498ada07257fa04042725c9
erin-day-2 <daye4@mail.uc.edu>
2022-02-16 11:18:24 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+	Scanner in = new Scanner(System.in);
+
+    double numF;
+    double numC;
+    String trash = "";
+
+
+    System.out.print("Please enter your Celsius: ");
+
+    if(in.hasNextDouble())
+    {
+        numC = in.nextDouble();
+        in.nextLine();
+        numF = (numC * 9/5) + 32;
+        System.out.println("The Fahrenheit is: " + numF);
+    }
+    else
+    {
+        trash = in.nextLine();
+        System.out.println("Illegal number: " + trash);
+        System.out.println("Exiting...run the program again!");
+        System.exit(0);
+    }
+
     }
 }
